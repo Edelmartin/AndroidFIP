4ac07c3c162f133a103cad39e2f3c31274576375
kfischer67 <fischerk067@gmail.com>
2015-11-10 10:44:54 +0100

    Récupération de la photo

--- app/src/main/java/com/example/bricola/app_test/AddNewMemberInGroupActivity.java
+++ app/src/main/java/com/example/bricola/app_test/AddNewMemberInGroupActivity.java
@@ -2,28 +2,37 @@ package com.example.bricola.app_test;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Contacts;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.telephony.TelephonyManager;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class AddNewMemberInGroupActivity extends AppCompatActivity {
@@ -35,10 +44,12 @@ public class AddNewMemberInGroupActivity extends AppCompatActivity {
     private Button addNewMemberRepertory = null;
     private Button deleteMemberButton = null;
     private EditText memberNameEditText = null;
+    private EditText memberNumberEditText = null;
     private String groupName = null;
     private static XMLManipulator groupXMLManipulator;
     private LinearLayout newMemberLinearLayout = null;
     private LinearLayout memberDetailsLinearLayout = null;
+    private ImageView imageviewcontact = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +58,11 @@ public class AddNewMemberInGroupActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         memberNameEditText = (EditText) findViewById(R.id.memberName_editText);
         newMemberLinearLayout = (LinearLayout) findViewById(R.id.newMember_linearLayout);
+
+        memberNumberEditText = (EditText) findViewById(R.id.memberNumber_editText);
 
         Bundle extras = getIntent().getExtras();
         groupName = extras.getString("groupName");
@@ -130,7 +144,8 @@ public class AddNewMemberInGroupActivity extends AppCompatActivity {
         addNewMemberRepertory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);//Contacts.People.CONTENT_URI);
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);//Contacts.People.CONTENT_URI); // creates the contact list intent
+                intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
                 startActivityForResult(intent, PICK_CONTACT);
             }
         });
@@ -184,44 +199,110 @@ public class AddNewMemberInGroupActivity extends AppCompatActivity {
             }
         });
     }
+
+
     @Override
-    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+   public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        Bitmap photo = null;
+        long monlong_picture_id = 0;
+        long monLong_id = 0;
+        String name_of_contacts = null;
+        String number_or_mail = null;
+
         super.onActivityResult(reqCode, resultCode, data);
         switch (reqCode) {
             case (PICK_CONTACT):
                 if (resultCode == Activity.RESULT_OK) {
-                    Uri contactData = data.getData();
+                    Uri contactData = data.getData(); // has the uri for picked contact
+                    //Uri contactPhotoUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, contactID);
                     ContentResolver cr = getContentResolver();
+                    ContentResolver photo_appli = getContentResolver();
                     ContentResolver crmail = getContentResolver();
-                    Cursor c = getContentResolver().query(contactData, null, null, null, null);
-                   /* if (c.moveToFirst()) {
-                        String id =
-                                c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));//Contacts.People.NAME));
-                        memberNameEditText.setText(id);
-                    }*/
+                    Cursor c = getContentResolver().query(contactData, null, null, null, null); // creates the contact cursor with the returned uri
 
                     while (c.moveToNext()) {
                         String id = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
                         String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                        String picture = c.getString(c.getColumnIndex(ContactsContract.Contacts.PHOTO_ID));
 
+                        monLong_id=Long.parseLong(id);
+
+                        if (picture!= null){
+                            monlong_picture_id=Long.parseLong(picture);
+                        }
+                        else {
+                            monlong_picture_id=Long.parseLong("0");
+                        }
                         if (Integer.parseInt(c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
                             Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
 
+
                             while (pCur.moveToNext()) {
                                 String phone = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                                memberNameEditText.setText(name + "   " + phone);
+
+                                //Replace +33 par 0
+                                phone = phone.replaceAll("\\+33","0");
+                                //Remove all non digit characters
+                                phone = phone.replaceAll("\\D+","");
+
+                                name_of_contacts= name;
+                                number_or_mail = phone;
                             }
                         }
                         else {
                             Cursor pCurmail = crmail.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,null,ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",new String[]{id},null);
                             while (pCurmail.moveToNext()) {
                                 String mail = pCurmail.getString(pCurmail.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
-                                memberNameEditText.setText(name + "   " + mail);
+                                name_of_contacts=name;
+                                number_or_mail=mail;
                             }
                         }
                     }
+                        photo = loadContactPhoto(photo_appli, monLong_id, monlong_picture_id);//301,2158);
+                        imageviewcontact = (ImageView) findViewById(R.id.img_contacts);
+                        imageviewcontact.setImageBitmap(photo);
+memberNameEditText.setText(name_of_contacts);
+memberNumberEditText.setText(number_or_mail);
                 }
                 break;
         }
     }
-}
+
+    public  Bitmap loadContactPhoto(ContentResolver cr, long  id,long photo_id)
+    {
+        Uri uri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, id);
+        InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(cr, uri);
+        if (input != null)
+        {
+            return BitmapFactory.decodeStream(input);
+        }
+        else
+        {
+            Log.d("PHOTO","first try failed to load photo");
+        }
+        byte[] photoBytes = null;
+
+        Uri photoUri = ContentUris.withAppendedId(ContactsContract.Data.CONTENT_URI, photo_id);
+
+        Cursor c = cr.query(photoUri, new String[] {ContactsContract.CommonDataKinds.Photo.PHOTO}, null, null, null);
+
+        try
+        {
+            if (c.moveToFirst())
+                photoBytes = c.getBlob(0);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+
+        } finally {
+
+            c.close();
+        }
+        if (photoBytes != null)
+            return BitmapFactory.decodeByteArray(photoBytes,0,photoBytes.length);
+        else
+            Log.d("PHOTO", "second try also failed");
+        return null;
+    }
+    }
